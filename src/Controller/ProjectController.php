c5c375f120a4f47eeb4ba0a46028a96692da167f
YDECORCE <y.decorce@codeur.online>
2021-01-26 15:52:23 +0100

    finalisation Dev uplaod images

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -9,7 +9,11 @@ use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\HttpFoundation\Response;
 // use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ProjectController extends AbstractController
 {
@@ -50,28 +54,73 @@ class ProjectController extends AbstractController
         $form=$this->createFormBuilder($project)
                     ->add('Title')
                     ->add('Description', CKEditorType::class)
-                    ->add('Image')
-                    ->add('Github')
-                    ->add('Weblink')
+                    ->add('Image', FileType::class, [
+                        'label' => 'Image',
+
+                // unmapped means that this field is not associated to any entity property
+                        'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                        'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                        'constraints' => [
+                                new File([
+                                    'maxSize' => '1024k',
+                                    'mimeTypes' => [
+                                    'image/png',
+                                    'image/jpg','image/jpeg','image/gif'
+                                        ],
+                        'mimeTypesMessage' => 'Merci d\'uploader un fichier image valide',
+                                ])
+                                    ],
+                    ])
+                    ->add('Github', UrlType::class)
+                    ->add('Weblink', UrlType::class)
                  
                     ->getForm();
         $form ->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $brochureFile=$form->get('Image')->getData();
+
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('Images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    return $this->redirectToRoute('admin');
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $project->setImage($newFilename);
+
+
             $Manager->persist($project);
             $Manager->flush();
 
             return $this->redirectToRoute('admin');
         }
-
+        }
       
         return $this->render('project/create.html.twig', [
                 'formProject' => $form->createView(),
                 'editmode' => $project->getId()!==null
 
         ]);
+    
     }
-
     
     /**
      * @Route("/project/{id}", name="project_show")
